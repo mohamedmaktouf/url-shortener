c75ae4e03a9c2063fa7460c9634a254362756cc1
Mohamed <mohamedmaktouf25@gmail.com>
2025-01-19 15:41:38 +0100

    feat(crud): add migration and model

---
+++ app/Models/Url.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property string $original_url
+ * @property string $shortened_url
+ */
+class Url extends Model
+{
+    //
+}
